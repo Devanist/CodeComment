6ebf213c984837fa78629b8565f9ac604f942af7
Devanist <Devanist@Devanist-PC.home>
2014-04-11 21:47:21 +0200

    Edited example

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+<?php
+    include 'codeComment.php'
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Test zamiany html na text</title>
+        <meta charset="UTF-8">
+        <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="codeComment.js"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="" method="POST">
+            Put your comment with html or another code between &lt;code&gt; tags.
+            <textarea id="t" name="a"></textarea>
+            <input type="submit" name="g" value="Click"/>
+        </form>
+        <hr>
+        <?php echo $text ?>
     </body>
 </html>
