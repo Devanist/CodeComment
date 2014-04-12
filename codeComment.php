4c6a52e992fb699f058134ae622ea6f06254d562
Devanist <Devanist@Devanist-PC.home>
2014-04-12 08:25:17 +0200

    cosmetic changes

--- codeComment.php
+++ codeComment.php
@@ -1,20 +1,23 @@
 <?php
-    $text = $_POST['a'];
-    $text = str_replace("<std", "&lt;std");
-    $text = str_replace("<!--" , "&lt;!--", $text);
-    $text = str_replace("<url", "&lt;url", $text);
-    $text = str_replace("</url", "&lt;/url", $text);
-    $text = str_replace("<meta", "&lt;meta", $text);
-    $text = str_replace("<html", "&lt;html", $text);
-    $text = str_replace("<head", "&lt;head", $text);
-    $text = str_replace("<body", "&lt;body", $text);
-    $text = str_replace("<title", "&lt;title", $text);
-    $text = str_replace("</title", "&lt;/title", $text);
-    $text = str_replace("</body", "&lt;/body", $text);
-    $text = str_replace("</html", "&lt;html", $text);
-    $text = str_replace("</head", "&lt;head", $text);
-    $text = str_replace("<!DOCTYPE", "&lt;!DOCTYPE", $text);
-    $text = str_replace("<?php", "&lt;?php", $text);
-    $text = str_replace("?>", "?&gt;", $text);
-    $_POST['a'] = $text;
+    if(isset($_POST['a'])){
+        $text = $_POST['a'];
+        $text = str_replace("<stdlib.h", "&lt;stdlib.h", $text);
+        $text = str_replace("<stdio.h", "&lt;stdio.h", $text);
+        $text = str_replace("<!--" , "&lt;!--", $text);
+        $text = str_replace("<url", "&lt;url", $text);
+        $text = str_replace("</url", "&lt;/url", $text);
+        $text = str_replace("<meta", "&lt;meta", $text);
+        $text = str_replace("<html", "&lt;html", $text);
+        $text = str_replace("<head", "&lt;head", $text);
+        $text = str_replace("<body", "&lt;body", $text);
+        $text = str_replace("<title", "&lt;title", $text);
+        $text = str_replace("</title", "&lt;/title", $text);
+        $text = str_replace("</body", "&lt;/body", $text);
+        $text = str_replace("</html", "&lt;html", $text);
+        $text = str_replace("</head", "&lt;head", $text);
+        $text = str_replace("<!DOCTYPE", "&lt;!DOCTYPE", $text);
+        $text = str_replace("<?php", "&lt;?php", $text);
+        $text = str_replace("?>", "?&gt;", $text);
+        $_POST['a'] = $text;
+    }
 ?>
